8a3a868fd8e4dcddbb43ddd74326fd3368893609
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-12-29 15:37:30 +0600

    update

--- netflix-clone/netflix-ui/src/components/NotAvailable.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotAvailable() {
-    return (
-        <h1 className="not-available">
-            Фильмы отсутствуют для выбранного жанра.
-        </h1>
-    )
-}
--- netflix-clone/netflix-ui/src/utils/constans.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = "93df875801bc9abfa3c2cf16c63b78a2";
-export const TMBD_BASE_URL = "http://api.themoviedb.org/3";
